a4bccbe2638c419e0bfb0f4891bfd0e5e747f7c9
frb <frb@MAC-510380.local>
2015-06-17 13:15:35 +0200

    ADD a mysql resoruces file

--- cosmos-gui/resources/mysql_db_and_tables.sql
+++ cosmos-gui/resources/mysql_db_and_tables.sql
@@ -2,3 +2,4 @@ DROP DATABASE cosmos_gui;
 CREATE DATABASE cosmos_gui;
 USE cosmos_gui;
 
+CREATE TABLE cosmos_user (idm_username VARCHAR(128) NOT NULL PRIMARY KEY UNIQUE, username TEXT NOT NULL, password TEXT NOT NULL, registrationTime TIMESTAMP NOT NULL);
