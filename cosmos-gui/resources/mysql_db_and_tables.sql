5c46922b98cf859827a5bba56949c3cdce9e54b5
frb <frb@MAC-510380.local>
2015-07-24 15:43:56 +0200

    UPDATE the MySQL database name

--- cosmos-gui/resources/mysql_db_and_tables.sql
+++ cosmos-gui/resources/mysql_db_and_tables.sql
@@ -1,5 +1,4 @@
-DROP DATABASE cosmos_gui;
-CREATE DATABASE cosmos_gui;
-USE cosmos_gui;
+CREATE DATABASE IF NOT EXISTS cosmos;
+USE cosmos;
 
 CREATE TABLE cosmos_user (idm_username VARCHAR(128) NOT NULL PRIMARY KEY UNIQUE, username TEXT NOT NULL, password TEXT NOT NULL, registration_time TIMESTAMP NOT NULL);
