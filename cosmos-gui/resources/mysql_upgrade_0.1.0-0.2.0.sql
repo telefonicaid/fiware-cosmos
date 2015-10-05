3d4b5ed9058b3b2b8048135f588ef79eab0e8ad3
frbattid <francisco.romerobueno@telefonica.com>
2015-10-05 13:27:01 +0200

    FIX issues raised during code review

--- cosmos-gui/resources/mysql_upgrade_0.1.0-0.2.0.sql
+++ cosmos-gui/resources/mysql_upgrade_0.1.0-0.2.0.sql
@@ -1,8 +1,10 @@
 USE cosmos;
 
+# 0.1.0 registration_time updates to current time always an update is performed, this line fixes it
 ALTER TABLE cosmos_user MODIFY registration_time TIMESTAMP DEFAULT "0000-00-00 00:00:00";
 ALTER TABLE cosmos_user ADD COLUMN last_access_time TIMESTAMP DEFAULT "0000-00-00 00:00:00";
 ALTER TABLE cosmos_user ADD COLUMN hdfs_used BIGINT NOT NULL;
 ALTER TABLE cosmos_user ADD COLUMN fs_used BIGINT NOT NULL;
 ALTER TABLE cosmos_user MODIFY hdfs_quota BIGINT NOT NULL;
+# 0.1.0 HDFS quota was measured in Gigabytes, now it is measured in bytes (1 GB = 1073741824 bytes)
 UPDATE cosmos_user SET hdfs_quota=1073741824*hdfs_quota;
