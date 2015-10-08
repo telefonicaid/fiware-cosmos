9b03f152133ceeae1d64864e1503246c3e025451
frbattid <francisco.romerobueno@telefonica.com>
2015-10-08 12:24:36 +0200

    ADD a unused accounts check

--- cosmos-admin/warnings.sh
+++ cosmos-admin/warnings.sh
@@ -21,7 +21,7 @@
 
 # Show the usage
 if [ $# -ne 8 ]; then
-        echo "Usage: ./warnings.sh <db_host> <db_port> <db_name> <db_user> <db_password> <hdfs_threshold> <destination_email> <report_title>"
+        echo "Usage: ./warnings.sh <db_host> <db_port> <db_name> <db_user> <db_password> <hdfs_threshold> <num_days> <destination_email> <report_title>"
         exit 1;
 fi
 
@@ -32,26 +32,49 @@ dbName=$3
 dbUser=$4
 dbPassword=$5
 hdfsThreshold=$6
-dstEmail=$7
-reportTitle=$8
+numDays=$7
+dstEmail=$8
+reportTitle=$9
 
-# Report
-report="### $reportTitle ###"
-
-# Detect users reaching the quota limit
-report="$report\n\n--- Cosmos users reaching the quota limit:"
+# Report pieces
+parametersReport="--- Parameterization:\ndb_host=$dbHost\ndb_port=$dbPort\ndb_name=$dbName\ndb_user=$dbUser\ndb_password=$dbPassword"
+parametersReport="$parametersReport\nhdfs_threshold=$hdfsThreshold\nnum_days=$numDays"
+quotaLimitReport="--- Users reaching the quota limit:"
+unusedAccountsReport="--- Unused accounts:"
 
 while read -r username; do
-        select_result=$(mysql $dbName -u $dbUser -p$dbPassword -se "select hdfs_quota,hdfs_used from cosmos_user where username='$username'")
+        # Common query
+        select_result=$(mysql $dbName -u $dbUser -p$dbPassword -se "select hdfs_quota,hdfs_used,registration_time from cosmos_user where username='$username'")
         hdfsQuota=$(echo $select_result | awk '{ print $1 }')
         hdfsUsed=$(echo $select_result | awk '{ print $2 }')
+        lastAccessTime=$(echo $select_result | awk '{ print $3 }')
+
+        # Detect users reaching the quota limit
         percentage=$(echo "scale=2;$hdfsUsed.0/$hdfsQuota.0" | bc)
-        check=$(echo "$percentage>$threshold" | bc)
+        check=$(echo "$percentage>$hdfsThreshold" | bc)
 
         if [ $check -eq 1 ]; then
-                report="$report\n$username: $hdfsQuota (quota) $hdfsUsed (used) $percentage (%)"
+                quotaLimitReport="$quotaLimitReport\n$username: $hdfsQuota (quota) $hdfsUsed (used) $percentage (%)"
+        fi
+
+        # Detect unused accounts
+        if [ "$lastAccessTime" == "0000-00-00" ]; then
+                continue;
+        fi
+
+        currentTimeSeconds=$(date +%s)
+        lastAccessTimeSeconds=$(date -d $lastAccessTime +%s)
+        check=$(echo "($currentTimeSeconds-$lastAccessTimeSeconds)>(86400*$numDays)" | bc)
+
+        if [ $check -eq 1 ]; then
+                if [ $hdfsUsed -eq 0 ]; then
+                        unusedAccountsReport="$unusedAccountsReport\n$username: $hdfsUsed (used) $lastAccessTime (last access time)"
+                fi
         fi
 done < <(mysql $dbName -u $dbUser -p$dbPassword -se "select username from cosmos_user")
+
+# Final report
+report="### $reportTitle ###\n\n$parametersReport\n\n$quotaLimitReport\n\n$unusedAccountsReport"
 
 # Send an email with the repport
 echo -e "$report" | mail -s "Cosmos report" $dstEmail
