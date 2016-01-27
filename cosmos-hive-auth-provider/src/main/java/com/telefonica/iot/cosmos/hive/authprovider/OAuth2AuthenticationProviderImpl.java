ca197a4d89507138925aadd8f3887693e2870cd4
frbattid <francisco.romerobueno@telefonica.com>
2016-01-27 10:29:18 +0100

    ADD OAuth2-based authentication provider for Hive

---
+++ cosmos-hive-auth-provider/src/main/java/com/telefonica/iot/cosmos/hive/authprovider/OAuth2AuthenticationProviderImpl.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright 2016 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-cosmos (FI-WARE project).
+ *
+ * fiware-cosmos is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * fiware-cosmos is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with fiware-cosmos. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please contact with iot_support at tid dot es
+ */
+package com.telefonica.iot.cosmos.hive.authprovider;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.security.sasl.AuthenticationException;
+import org.apache.hive.service.auth.PasswdAuthenticationProvider;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ * Custom implementation of the org.apache.hive.service.auth.PasswdAuthenticationProvider interface.
+ * 
+ * @author frb
+ */
+public class OAuth2AuthenticationProviderImpl implements PasswdAuthenticationProvider {
+    
+    private static final Logger LOGGER = Logger.getLogger(HttpClientFactory.class);
+    private final HttpClientFactory httpClientFactory;
+    
+    /**
+     * Constructor.
+     */
+    public OAuth2AuthenticationProviderImpl() {
+        httpClientFactory = new HttpClientFactory(true);
+    } // OAuth2AuthenticationProviderImpl
+
+    @Override
+    public void Authenticate(String user, String password) throws AuthenticationException {
+        // create the Http client
+        HttpClient httpClient = httpClientFactory.getHttpClient(true);
+        
+        // create the request
+        String url = "https://account.lab.fiware.org/user?access_token=" + password;
+        HttpRequestBase request = new HttpGet(url);
+        
+        // do the request
+        HttpResponse httpRes = null;
+        
+        try {
+            httpRes = httpClient.execute(request);
+            LOGGER.debug("Doing request: " + request.toString());
+        } catch (IOException e) {
+            throw new AuthenticationException(e.getMessage());
+        } // try catch
+        
+        // get the input streamResponse
+        String streamResponse = "";
+        
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpRes.getEntity().getContent()));
+            streamResponse = reader.readLine();
+            LOGGER.debug("Response received: " + streamResponse);
+        } catch (IOException e) {
+            throw new AuthenticationException(e.getMessage());
+        } // try catch
+        
+        // parse the input streamResponse as a Json
+        JSONObject jsonResponse = null;
+        
+        try {
+            JSONParser jsonParser = new JSONParser();
+            jsonResponse = (JSONObject) jsonParser.parse(streamResponse);
+        } catch (ParseException e) {
+            throw new AuthenticationException(e.getMessage());
+        } // try catch
+        
+        // check if the given token does not exist
+        if (jsonResponse.containsKey("error")) {
+            throw new AuthenticationException("The given token does not exist");
+        } // if
+        
+        // check if the obtained user id matches the given user
+        if (jsonResponse.containsKey("id") && !jsonResponse.get("id").equals(user)) {
+            throw new AuthenticationException("The given token does not match the given user");
+        } // if
+    } // Authenticate
+    
+} // OAuth2AuthenticationProviderImpl
---
+++ cosmos-hive-auth-provider/src/test/java/com/telefonica/iot/cosmos/hive/authprovider/OAuth2AuthenticationProviderImplTest.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2016 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-cosmos (FI-WARE project).
+ *
+ * fiware-cosmos is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * fiware-cosmos is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with fiware-cosmos. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please contact with iot_support at tid dot es
+ */
+package com.telefonica.iot.cosmos.hive.authprovider;
+
+import javax.security.sasl.AuthenticationException;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ * Suite of tests for OAuth2AuthenticationProviderImpl.
+ * 
+ * @author frb
+ */
+public class OAuth2AuthenticationProviderImplTest {
+    
+    private final String matchingUser = "frb";
+    private final String notMatchingUser = "other";
+    private final String matchingToken = "8azH35GGPdapmgVCODLOtxy52f6a8o";
+    private final String notMatchingToken = "000000000000000000000000000000";
+    
+    /**
+     * Tests OAuth2AuthenticationProviderImpl.Authenticate when the given user and token match.
+     */
+    @Test
+    public void testAuthenticateSuccess() {
+        System.out.println("Testing OAuth2AuthenticationProviderImpl.Authenticate, it must success");
+        
+        try {
+            OAuth2AuthenticationProviderImpl oapi = new OAuth2AuthenticationProviderImpl();
+            oapi.Authenticate(matchingUser, matchingToken);
+        } catch (AuthenticationException e) {
+            fail(e.getMessage());
+        } finally {
+            assertTrue(true);
+        } // try catch finally
+    } // testAuthenticateSuccess
+    
+    /**
+     * Tests OAuth2AuthenticationProviderImpl.Authenticate when the given user and token don't match.
+     */
+    @Test
+    public void testAuthenticateFail() {
+        System.out.println("Testing OAuth2AuthenticationProviderImpl.Authenticate, "
+                + "it must fail because the user and the token don't match");
+        
+        try {
+            OAuth2AuthenticationProviderImpl oapi = new OAuth2AuthenticationProviderImpl();
+            oapi.Authenticate(notMatchingUser, matchingToken);
+            assertTrue(false);
+        } catch (AuthenticationException e) {
+            assertTrue(true);
+        } // try catch
+    } // testAuthenticateFail
+    
+    /**
+     * Tests OAuth2AuthenticationProviderImpl.Authenticate when the given token does not exist.
+     */
+    @Test
+    public void testAuthenticateUnexistentToken() {
+        System.out.println("Testing OAuth2AuthenticationProviderImpl.Authenticate, "
+                + "it must fail because the token does not exist");
+        
+        try {
+            OAuth2AuthenticationProviderImpl oapi = new OAuth2AuthenticationProviderImpl();
+            oapi.Authenticate(matchingUser, notMatchingToken);
+            assertTrue(false);
+        } catch (AuthenticationException e) {
+            assertTrue(true);
+        } // try catch
+    } // testAuthenticateUnexistentToken
+    
+} // OAuth2AuthenticationProviderImplTest
