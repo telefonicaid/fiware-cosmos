da5d826066b9fd37e9faad4796c02b889b8a8e50
frbattid <francisco.romerobueno@telefonica.com>
2016-03-03 10:33:27 +0100

    ADD runHadoopJar and runKill functions to cmd_runner.js

--- cosmos-tidoop-api/src/cmd_runner.js
+++ cosmos-tidoop-api/src/cmd_runner.js
@@ -1,16 +1,16 @@
 /**
  * Copyright 2016 Telefonica Investigación y Desarrollo, S.A.U
  *
- * This file is part of fiware-tidoop (FI-WARE project).
+ * This file is part of fiware-cosmos (FI-WARE project).
  *
- * fiware-tidoop is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * fiware-cosmos is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * fiware-tidoop is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * fiware-cosmos is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
- * You should have received a copy of the GNU Affero General Public License along with fiware-tidoop. If not, see
+ * You should have received a copy of the GNU Affero General Public License along with fiware-cosmos. If not, see
  * http://www.gnu.org/licenses/.
  *
  * For those usages not covered by the GNU Affero General Public License please contact with
@@ -27,8 +27,9 @@
 var spawn = require('child_process').spawn;
 var mysqlDriver = require('./mysql_driver.js');
 
-function run(jobId, cmd, params, callback) {
-    var job = spawn(cmd, params);
+function runHadoopJar(jobId, jarPath, className, jarPath, inputData, outputData, callback) {
+    var params = ['jar', jarPath, className, '-libjars', jarPath, inputData, outputData];
+    var job = spawn('hadoop', params);
     var result = '';
 
     job.stdout.on('data', function (data) {
@@ -57,8 +58,13 @@ function run(jobId, cmd, params, callback) {
     job.on('close', function (code) {
         return callback(null, result);
     });
-} // run
+} // runHadoopJar
+
+function runKill(jobId, callback) {
+    // TBD
+} // runKill
 
 module.exports = {
-    run: run
+    runHadoopJar: runHadoopJar,
+    runKill: runKill
 } // module.exports
